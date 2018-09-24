c6674668498c6bf9ea71740d92d677406d8f7a28
ross daly <rossdaly@gmail.com>
2018-09-24 20:46:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,17 +15,17 @@ function init() {
   const main = document.querySelector('html')
   let count=0
   main.addEventListener('keydown', function(event) {
-   // console.log(event.key)
-   if (event.key==codes[count]){
-     count++
-     // console.log(`${count} / ${codes.length}`)
-     if (count === codes.length){
-       window.alert("YOU DID IT!")
-       count = 0
-     }
-  } else {
-    count = 0
-  }
+     // console.log(event.key)
+     if (event.key==codes[count]){
+       count++
+       // console.log(`${count} / ${codes.length}`)
+       if (count === codes.length){
+         window.alert("YOU DID IT!")
+         count = 0
+       }
+    } else {
+      count = 0
+    }
   })
 }
 
