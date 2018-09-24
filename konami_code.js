96d2b09aab50aa2ff82a83616aa0a0b1b9c22ab7
ross daly <rossdaly@gmail.com>
2018-09-24 14:11:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,8 +13,16 @@ const codes = [
 
 function init() {
   const main = document.querySelector('body')
- 
+ let count=0
 main.addEventListener('click', function(event) {
-  alert('I was clicked!')
+  if (event.which==codes[count]){
+    count++
+  } else {
+    count =0
+  }
+  if (count === codes.length){
+    alert('you done did it!!!')
+  }
+  // alert('I was clicked!')
 })
 }
